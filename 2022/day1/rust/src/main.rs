0b6e3830b7b6b629da3f07a2033c875d5f9f767d
Antoine Romero-Romero <ant.romero2@orange.fr>
2022-12-01 23:20:34 +0000

    feat(day1): add second problem solution

--- 2022/day1/rust/src/main.rs
+++ 2022/day1/rust/src/main.rs
@@ -2,7 +2,17 @@ use std::{error::Error, fs};
 
 fn main() {
     let data = get_input().unwrap();
-    let lines: Vec<&str> = data.split("\n").collect();
+    part_one(&data);
+    part_two(&data);
+}
+
+fn get_input() -> Result<String, Box<dyn Error>> {
+    let data = fs::read_to_string("./data/data.txt")?;
+    Ok(data)
+}
+
+fn part_one(raw_data: &str) {
+    let lines: Vec<&str> = raw_data.split("\n").collect();
     let mut max = 0;
     let mut current_sum = 0;
     for line in lines {
@@ -18,7 +28,19 @@ fn main() {
     println!("Result is {max}");
 }
 
-fn get_input() -> Result<String, Box<dyn Error>> {
-    let data = fs::read_to_string("./data/data.txt")?;
-    Ok(data)
+fn part_two(raw_data: &str) {
+    let lines: Vec<&str> = raw_data.split("\n").collect();
+    let mut sums = vec![];
+    let mut current_sum = 0;
+    for line in lines {
+        if line.is_empty() {
+            sums.push(current_sum);
+            current_sum = 0;
+            continue;
+        }
+        current_sum += line.parse::<i32>().unwrap();
+    }
+    sums.sort_unstable_by(|a, b| b.cmp(a));
+    let result: i32 = sums.iter().take(3).sum();
+    println!("Result is {result}");
 }
