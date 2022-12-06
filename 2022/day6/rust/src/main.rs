bf3be77580f0de34e658aa1058bfedae68af71b2
Antoine Romero-Romero <ant.romero2@orange.fr>
2022-12-06 22:55:35 +0000

    feat(day6): add first problem solution

---
+++ 2022/day6/rust/src/main.rs
@@ -0,0 +1,31 @@
+use std::{collections::HashSet, error::Error, fs};
+
+fn main() {
+    let data = get_input().unwrap();
+    part_one(&data);
+    part_two(&data);
+}
+
+fn get_input() -> Result<String, Box<dyn Error>> {
+    let data = fs::read_to_string("./data/data.txt")?;
+    Ok(data)
+}
+
+fn part_one(raw_data: &str) {
+    let lines: Vec<&str> = raw_data.split("\n").collect();
+    let line = lines[0];
+    let result = line
+        .as_bytes()
+        .windows(4)
+        .enumerate()
+        .skip_while(|chunk| {
+            chunk.1.len() != HashSet::<u8>::from_iter(chunk.1.iter().cloned()).len()
+        })
+        .next()
+        .unwrap()
+        .0
+        + 4;
+    println!("Part 1 result is {result}")
+}
+
+fn part_two(raw_data: &str) {}
