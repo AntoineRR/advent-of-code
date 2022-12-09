11551ea936013b56939132a83f98c9cdbb50f109
Antoine Romero-Romero <ant.romero2@orange.fr>
2022-12-09 21:24:22 +0000

    feat(day9): add first problem solution

---
+++ 2022/day9/rust/src/main.rs
@@ -0,0 +1,94 @@
+use std::{collections::HashSet, error::Error, fs};
+
+fn main() {
+    let data = get_input().unwrap();
+    part_one(&data);
+    part_two(&data);
+}
+
+fn get_input() -> Result<String, Box<dyn Error>> {
+    let data = fs::read_to_string("./data/data.txt")?;
+    Ok(data)
+}
+
+fn display_pos(tail_pos: (i32, i32), head_pos: (i32, i32)) {
+    let relative_pos = (tail_pos.0 - head_pos.0, tail_pos.1 - head_pos.1);
+    if relative_pos.0 > 1 || relative_pos.0 < -1 || relative_pos.1 > 1 || relative_pos.1 < -1 {
+        println!("no");
+    }
+    let mut to_print = "".to_string();
+    for i in -1..2 {
+        for j in -1..2 {
+            if relative_pos.0 == i && relative_pos.1 == j {
+                to_print += "T";
+            } else if i == 0 && j == 0 {
+                to_print += "H";
+            } else {
+                to_print += " ";
+            }
+        }
+        to_print += "\n";
+    }
+    println!("{to_print}");
+}
+
+fn part_one(raw_data: &str) {
+    let lines: Vec<&str> = raw_data.split("\n").collect();
+    let mut visited = HashSet::new();
+    let mut head_pos = (0, 0);
+    let mut tail_pos = (0, 0);
+    for line in lines {
+        let (dir, amount) = line.split_once(' ').unwrap();
+        let amount = amount.parse().unwrap();
+        for _ in 0..amount {
+            if dir == "R" {
+                head_pos.1 += 1;
+            } else if dir == "U" {
+                head_pos.0 -= 1;
+            } else if dir == "L" {
+                head_pos.1 -= 1;
+            } else {
+                head_pos.0 += 1;
+            }
+            if head_pos.0 - tail_pos.0 >= 2 {
+                tail_pos.0 += 1;
+                if head_pos.1 - tail_pos.1 >= 1 {
+                    tail_pos.1 += 1;
+                } else if head_pos.1 - tail_pos.1 <= -1 {
+                    tail_pos.1 -= 1;
+                }
+            } else if head_pos.0 - tail_pos.0 <= -2 {
+                tail_pos.0 -= 1;
+                if head_pos.1 - tail_pos.1 >= 1 {
+                    tail_pos.1 += 1;
+                } else if head_pos.1 - tail_pos.1 <= -1 {
+                    tail_pos.1 -= 1;
+                }
+            }
+            if head_pos.1 - tail_pos.1 >= 2 {
+                tail_pos.1 += 1;
+                if head_pos.0 - tail_pos.0 >= 1 {
+                    tail_pos.0 += 1;
+                } else if head_pos.0 - tail_pos.0 <= -1 {
+                    tail_pos.0 -= 1;
+                }
+            } else if head_pos.1 - tail_pos.1 <= -2 {
+                tail_pos.1 -= 1;
+                if head_pos.0 - tail_pos.0 >= 1 {
+                    tail_pos.0 += 1;
+                } else if head_pos.0 - tail_pos.0 <= -1 {
+                    tail_pos.0 -= 1;
+                }
+            }
+            if !visited.contains(&tail_pos) {
+                visited.insert(tail_pos);
+            }
+            // println!("{}, head: {:?}, tail: {:?}", line, head_pos, tail_pos);
+            // display_pos(tail_pos, head_pos);
+        }
+    }
+    let result = visited.len();
+    println!("Part 1 result is {result}")
+}
+
+fn part_two(raw_data: &str) {}
