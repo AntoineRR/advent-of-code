3de87e08e24ddc2a6b1f619bfee475fde47ee209
Antoine Romero-Romero <ant.romero2@orange.fr>
2022-12-10 20:35:08 +0000

    feat(day10): add second problem solution

--- 2022/day10/rust/src/main.rs
+++ 2022/day10/rust/src/main.rs
@@ -35,7 +35,41 @@ fn part_one(raw_data: &str) {
             x += to_add;
         }
     }
-    println!("Part 1 result is {result}")
+    println!("Part 1 result is {result}");
 }
 
-fn part_two(raw_data: &str) {}
+fn get_char_to_draw(cycles: i32, x: i32) -> char {
+    if (cycles % 40) - x <= 1 && (cycles % 40) - x >= -1 {
+        '#'
+    } else {
+        '.'
+    }
+}
+
+fn part_two(raw_data: &str) {
+    let lines: Vec<&str> = raw_data.split("\n").collect();
+    let mut x = 1;
+    let mut cycles = 0;
+    let mut result = "".to_string();
+    for line in lines {
+        if line.contains("noop") {
+            result += &get_char_to_draw(cycles, x).to_string();
+            cycles += 1;
+        } else {
+            let (_, to_add) = line.split_once(' ').unwrap();
+            let to_add: i32 = to_add.parse().unwrap();
+            result += &get_char_to_draw(cycles, x).to_string();
+            cycles += 1;
+            if cycles % 40 == 0 {
+                result += "\n";
+            }
+            result += &get_char_to_draw(cycles, x).to_string();
+            cycles += 1;
+            x += to_add;
+        }
+        if cycles % 40 == 0 {
+            result += "\n";
+        }
+    }
+    println!("Part 2 result is:\n{result}");
+}
