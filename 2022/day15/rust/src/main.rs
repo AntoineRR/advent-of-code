03ef6145c3d76a5ce35b9ffe9d7252c3c8d9a208
Antoine Romero-Romero <ant.romero2@orange.fr>
2022-12-15 21:09:19 +0000

    feat(day15): add second problem solution

--- 2022/day15/rust/src/main.rs
+++ 2022/day15/rust/src/main.rs
@@ -76,4 +76,85 @@ fn part_one(raw_data: &str) {
     println!("Part 1 result is {result}")
 }
 
-fn part_two(raw_data: &str) {}
+fn part_two(raw_data: &str) {
+    let lines: Vec<&str> = raw_data.split("\n").collect();
+    let sources: Vec<SourceWithBeacon> = lines
+        .iter()
+        .map(|l| SourceWithBeacon::from_line(l))
+        .collect();
+    let mut coords = (0, 0);
+    for line_y in 0..=4_000_000 {
+        let mut intervals_covered = vec![];
+        for source in &sources {
+            let distance_from_beacon = get_manhattan_distance(source.source, source.closest_beacon);
+            let distance_from_line =
+                get_manhattan_distance(source.source, (source.source.0, line_y));
+            if distance_from_beacon >= distance_from_line {
+                let diff: i32 = (distance_from_beacon - distance_from_line)
+                    .try_into()
+                    .unwrap();
+                let start = source.source.0 - diff;
+                let end = source.source.0 + diff;
+                intervals_covered.push((start, end));
+            }
+        }
+        let result = intervals_covered
+            .iter()
+            .enumerate()
+            .filter(|(_, (min, max))| *min <= 4_000_000 || *max >= 0)
+            .next()
+            .unwrap();
+        let mut current_interval = result.1.clone();
+        let mut rest = intervals_covered.clone();
+        rest.remove(result.0);
+
+        let mut found = false;
+        loop {
+            let rest_len = rest.len();
+            for i in 0..rest.len() {
+                if !(current_interval.1 < rest[i].0 || rest[i].1 < current_interval.0) {
+                    current_interval = (
+                        current_interval.0.min(rest[i].0),
+                        current_interval.1.max(rest[i].1),
+                    );
+                    rest.remove(i);
+                    break;
+                }
+            }
+            if current_interval.0 <= 0 && current_interval.1 >= 4_000_000 {
+                break;
+            }
+            if rest.len() == 0 {
+                break;
+            } else if rest_len == rest.len() {
+                if current_interval.1 < 0 || current_interval.0 > 4_000_000 {
+                    break;
+                }
+                found = true;
+                break;
+            }
+        }
+
+        if found {
+            coords.1 = line_y;
+
+            let mins: Vec<i32> = intervals_covered.iter().map(|(m, _)| *m).collect();
+            let maxs: Vec<i32> = intervals_covered.iter().map(|(_, m)| *m + 1).collect();
+            let mut intensity: i32 = mins.iter().filter(|&m| *m < 0).count() as i32
+                - maxs.iter().filter(|&m| *m < 0).count() as i32;
+            for i in 0..=4_000_000 {
+                let min_count = mins.iter().filter(|&m| *m == i).count() as i32;
+                let max_count = maxs.iter().filter(|&m| *m == i).count() as i32;
+                intensity += min_count - max_count;
+                if intensity <= 0 {
+                    coords.0 = i;
+                }
+            }
+            if coords.0 != 0 {
+                break;
+            }
+        }
+    }
+    let result = (coords.0 as u64) * 4_000_000 + (coords.1 as u64);
+    println!("Part 2 result is {result}")
+}
