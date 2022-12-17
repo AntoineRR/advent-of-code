3e8243f40cae246ec758e4ab519e7e3b0073b0b1
Antoine Romero-Romero <ant.romero2@orange.fr>
2022-12-17 23:26:47 +0000

    feat(day17): part two attempt

--- 2022/day17/rust/src/main.rs
+++ 2022/day17/rust/src/main.rs
@@ -1,4 +1,6 @@
-use std::{error::Error, fmt::Display, fs};
+// Part two doesn't work for now!
+
+use std::{collections::HashSet, error::Error, fmt::Display, fs};
 
 fn main() {
     let data = get_input().unwrap();
@@ -95,12 +97,13 @@ impl Rock {
         self.offset = offset;
         Ok(())
     }
-
-    fn get_max_height(&self) -> u64 {
-        self.offset
-    }
 }
 
+fn is_line_useless(upper: &[u8]) -> bool {
+    upper.iter().fold(0, |acc, l| acc | l) == 0b11111110
+}
+
+#[derive(Clone)]
 struct OccupiedLines {
     occupied: Vec<u8>,
     offset: u64,
@@ -118,23 +121,24 @@ impl OccupiedLines {
             } else {
                 self.occupied.push(*cell);
                 self.offset += 1;
-                if self.occupied.len() > 100 {
-                    self.occupied.remove(0);
-                }
             }
+        }
+        while is_line_useless(&self.occupied[1..]) {
+            self.occupied.remove(0);
         }
     }
 }
 
 fn display(rocks: &OccupiedLines, highest: u64, current: Option<&Rock>) {
     let mut lines = vec![];
-    for _ in 0..highest + 10 {
+    let low = highest - rocks.occupied.len() as u64;
+    for _ in low..highest + 10 {
         lines.push(vec!['|', '.', '.', '.', '.', '.', '.', '.', '|']);
     }
     for (i, rock) in rocks.occupied.iter().rev().enumerate() {
         for j in 0..8 {
             if (rock << j) & 0b10000000 > 1 {
-                lines[rocks.offset as usize - i][j + 1] = '#';
+                lines[rocks.offset as usize - i - low as usize][j + 1] = '#';
             }
         }
     }
@@ -142,7 +146,7 @@ fn display(rocks: &OccupiedLines, highest: u64, current: Option<&Rock>) {
         for (i, cell) in r.cells.iter().enumerate() {
             for j in 0..8 {
                 if (cell << j) & 0b10000000 > 1 {
-                    lines[r.offset as usize - i][j + 1] = '@';
+                    lines[r.offset as usize - i - low as usize][j + 1] = '@';
                 }
             }
         }
@@ -157,30 +161,23 @@ fn display(rocks: &OccupiedLines, highest: u64, current: Option<&Rock>) {
     );
 }
 
-fn part_one(raw_data: &str) {
-    let shift_sequence = get_shift_sequence(raw_data);
-    let rock_sequence = vec![
-        Rock::from_cells(vec![0b11110000]),
-        Rock::from_cells(vec![0b01000000, 0b11100000, 0b01000000]),
-        Rock::from_cells(vec![0b00100000, 0b00100000, 0b11100000]),
-        Rock::from_cells(vec![0b10000000, 0b10000000, 0b10000000, 0b10000000]),
-        Rock::from_cells(vec![0b11000000, 0b11000000]),
-    ];
-    let mut result = 0;
-    let mut current_rock = 0;
-    let mut current_shift = 0;
-    let mut occupied_cells = OccupiedLines {
-        occupied: vec![0b11111110],
-        offset: 0,
-    };
-
-    for _ in 0..1_000_000 {
-        let mut rock = rock_sequence[current_rock].clone();
-        rock.set_spawn(result);
+fn get_height(
+    rock_sequence: &[Rock],
+    shift_sequence: &[Shift],
+    n_rocks: usize,
+    current_rock: &mut usize,
+    current_shift: &mut usize,
+    occupied_cells: &mut OccupiedLines,
+    patterns_to_match: Option<HashSet<(Vec<u8>, usize, usize)>>,
+    encountered: &mut Option<HashSet<Vec<u8>>>,
+) -> Option<usize> {
+    for i in 0..n_rocks {
+        let mut rock = rock_sequence[*current_rock].clone();
+        rock.set_spawn(occupied_cells.offset);
         //display(&occupied_cells, result, Some(&rock));
         loop {
-            let shift = shift_sequence[current_shift].clone();
-            current_shift = (current_shift + 1) % shift_sequence.len();
+            let shift = shift_sequence[*current_shift].clone();
+            *current_shift = (*current_shift + 1) % shift_sequence.len();
             let mut direction = match shift {
                 Shift::Left => Direction::Left,
                 Shift::Right => Direction::Right,
@@ -195,15 +192,113 @@ fn part_one(raw_data: &str) {
         }
 
         occupied_cells.add_rock(&rock);
-        let max_in_rock = rock.get_max_height();
-        if max_in_rock > result {
-            result = max_in_rock;
+        if let Some(e) = encountered {
+            e.insert(occupied_cells.occupied.clone());
         }
-
-        current_rock = (current_rock + 1) % rock_sequence.len();
+        if let Some(pattern) = &patterns_to_match {
+            if pattern.contains(&(
+                occupied_cells.occupied.clone(),
+                *current_rock,
+                *current_shift,
+            )) {
+                println!("found it!");
+                return Some(i);
+            }
+        }
+        *current_rock = (*current_rock + 1) % rock_sequence.len();
     }
+    None
+}
+
+fn part_one(raw_data: &str) {
+    let shift_sequence = get_shift_sequence(raw_data);
+    let rock_sequence = vec![
+        Rock::from_cells(vec![0b11110000]),
+        Rock::from_cells(vec![0b01000000, 0b11100000, 0b01000000]),
+        Rock::from_cells(vec![0b00100000, 0b00100000, 0b11100000]),
+        Rock::from_cells(vec![0b10000000, 0b10000000, 0b10000000, 0b10000000]),
+        Rock::from_cells(vec![0b11000000, 0b11000000]),
+    ];
+    let mut current_rock = 0;
+    let mut current_shift = 0;
+    let mut occupied_cells = OccupiedLines {
+        occupied: vec![0b11111110],
+        offset: 0,
+    };
+    get_height(
+        &rock_sequence,
+        &shift_sequence,
+        2022,
+        &mut current_rock,
+        &mut current_shift,
+        &mut occupied_cells,
+        None,
+        &mut None,
+    );
+    let result = occupied_cells.offset;
 
     println!("Part 1 result is {result}")
 }
 
-fn part_two(raw_data: &str) {}
+fn part_two(raw_data: &str) {
+    let shift_sequence = get_shift_sequence(raw_data);
+    let rock_sequence = vec![
+        Rock::from_cells(vec![0b11110000]),
+        Rock::from_cells(vec![0b01000000, 0b11100000, 0b01000000]),
+        Rock::from_cells(vec![0b00100000, 0b00100000, 0b11100000]),
+        Rock::from_cells(vec![0b10000000, 0b10000000, 0b10000000, 0b10000000]),
+        Rock::from_cells(vec![0b11000000, 0b11000000]),
+    ];
+    let mut current_rock = 0;
+    let mut current_shift = 0;
+    let mut occupied_cells = OccupiedLines {
+        occupied: vec![0b11111110],
+        offset: 0,
+    };
+    let encountered = HashSet::new();
+    get_height(
+        &rock_sequence,
+        &shift_sequence,
+        1_000_000,
+        &mut current_rock,
+        &mut current_shift,
+        &mut occupied_cells,
+        None,
+        &mut Some(encountered.clone()),
+    );
+
+    display(&occupied_cells, occupied_cells.offset, None);
+    let pattern_to_match = Some(
+        encountered
+            .iter()
+            .map(|e| (e.clone(), current_rock, current_shift))
+            .collect::<HashSet<(Vec<u8>, usize, usize)>>(),
+    );
+    let n = get_height(
+        &rock_sequence,
+        &shift_sequence,
+        1_000_000_000,
+        &mut current_rock,
+        &mut current_shift,
+        &mut occupied_cells,
+        pattern_to_match,
+        &mut None,
+    )
+    .unwrap();
+    let height = occupied_cells.offset;
+    let mut result = height * (1_000_000_000_000 / (n - 1_000_000)) as u64;
+    let rest = 1_000_000_000_000 / (n - 1_000_000);
+    get_height(
+        &rock_sequence,
+        &shift_sequence,
+        rest,
+        &mut current_rock,
+        &mut current_shift,
+        &mut occupied_cells,
+        None,
+        &mut None,
+    );
+    result += occupied_cells.offset - height;
+
+    println!("Part 2 result is {result}")
+}
